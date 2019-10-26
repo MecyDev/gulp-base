d3d6353e9ebce589657d3e119d40a6cfca601b81
MecyDev <macmc@tutanota.com>
2019-10-27 00:08:43 +0200

    correction fichiers src

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 function style() {
-    return gulp.src('./src/scss/**/**/*.scss')
+    return gulp.src('./src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
@@ -15,7 +15,7 @@ function watch() {
             baseDir: './dist'
         }
     });
-    gulp.watch('./src/scss/**/**/*.scss', style);
+    gulp.watch('./src/scss/**/*.scss', style);
     gulp.watch('./dist/*.html').on('change', browserSync.reload);
 }
 
