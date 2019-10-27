414846519ad9abdff30a13c4f6c58b9535289cd8
MecyDev <macmc@tutanota.com>
2019-10-27 18:07:56 +0100

    ajout fonction image simple et correction mineurs

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,27 @@ const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const del = require('del');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
 function clean() {
-    return del("./dir/css");
+    return del('./dir/css');
 }
 
 function style() {
     return gulp.src('./src/scss/**/*.scss', { sourcemaps: true })
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'))
-        .pipe(rename({ basename: "styles", suffix: ".min" }))
+        .pipe(rename({ basename: 'styles', suffix: '.min' }))
         .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(gulp.dest("./dist/css", { sourcemaps: '.' }))
+        .pipe(gulp.dest('./dist/css', { sourcemaps: '.' }))
         .pipe(browserSync.stream());
+}
+
+function image() {
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
 }
 
 function watch() {
@@ -33,4 +40,5 @@ function watch() {
 
 exports.default = gulp.parallel(clean, style);
 exports.style = style;
+exports.image = image;
 exports.watch = watch;
