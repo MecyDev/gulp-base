2a906fab8ff65a5cb60873aaefeae737e16c6d27
MecyDev <macmc@tutanota.com>
2019-10-27 02:55:47 +0200

    suppression sourcemap et ajout tratement sur css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require("autoprefixer");
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const del = require('del');
+const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
 
+function clean() {
+    return del("./dir/css");
+}
+
 function style() {
-    return gulp.src('./src/scss/**/*.scss', { sourcemaps: true })
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(plumber())
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css', { sourcemaps: '.' }))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(rename({ basename: "styles", suffix: ".min" }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(gulp.dest("./dist/css"))
         .pipe(browserSync.stream());
 }
 
@@ -19,5 +33,6 @@ function watch() {
     gulp.watch('./dist/*.html').on('change', browserSync.reload);
 }
 
+exports.default = gulp.parallel(clean, style);
 exports.style = style;
 exports.watch = watch;
